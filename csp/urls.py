4a10a181edd36b77064eb6ce5ea6e7a498b758ae
Xavier-Planes <xplanes123@gmail.com>
2016-04-27 20:52:19 +0200

    Update urls.py

--- csp/urls.py
+++ csp/urls.py
@@ -14,6 +14,7 @@ from django.contrib.auth.forms import UserCreationForm
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', mainpage),
+    url(r'^cspapp/', include('cspapp.urls', namespace = 'cspapp')),
     url(r'^accounts/login/$', login, name='login'),
     url(r'^accounts/logout/$', logout, name='logout'),
 
@@ -23,3 +24,8 @@ urlpatterns = [
         success_url='http://127.0.0.1:8000/'),name='register'),
 ]
 
+if settings.DEBUG:
+    urlpatterns += [
+        url(r'^media/(?P<path>.*)$', 'django.views.static.serve',
+            {'document_root': settings.MEDIA_ROOT, })
+    ]
