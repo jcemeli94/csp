b64757aeaf66beef93ffae82e87b37e9a39dd410
Xavier-Planes <xplanes123@gmail.com>
2016-04-27 20:57:50 +0200

    Update forms.py

--- cspapp/forms.py
+++ cspapp/forms.py
@@ -1,24 +1,15 @@
-<<<<<<< HEAD
 from django.forms import ModelForm
 from models import *
-
-
-class ProjectForm(ModelForm):
-    class Meta:
-        model = Project
-        exclude = ()
-
-=======
 from django import forms
-from .models import *
+
 
 
 class ProjectForm(ModelForm):
     class Meta:
         model = Project
         exclude = ()
-        
-        
+
+
 class PostForm(forms.ModelForm):
 
         class Meta:
@@ -26,5 +17,5 @@ class PostForm(forms.ModelForm):
             fields = ('username','password','email',)
 
 #NO SE USA EN EL REGISTER, SERVIRA PARA INSERTAR POSTS
->>>>>>> origin/master
+
 
